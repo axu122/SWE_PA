d6ba46f1aeba9b7a2960557492ea28f9ede62c9d
David Kim <kimbyi@bc.edu>
2020-04-17 11:30:15 -0400

    defined relationships in model. Needs modifications

--- DLL_Delivery4/peer_assessment/models.py
+++ DLL_Delivery4/peer_assessment/models.py
@@ -41,16 +41,14 @@ class User(models.Model):
     type = models.CharField(max_length=1, choices=USER_TYPES, null=False)
 
 
-class GroupOne(models.Model):
-    def validate_digit_length(eagle_id):
-        if not (eagle_id.isdigit() and len(eagle_id) == 8):
-            raise ValidationError('%(id)s must be 8 digits',
-                                  params={'id': eagle_id}, )
 
-    eagle_id = models.CharField(verbose_name="id", max_length=8, validators=[
-                                validate_digit_length], null=False)
+################################################
+
+class GroupOne(models.Model):
+    eagle_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group,on_delete=models.CASCADE)
+
     key_1 = models.CharField(max_length=100, primary_key=True)
-    group_id = models.CharField(max_length=100, unique=False)
 
 
 class Group(models.Model):
@@ -60,22 +58,25 @@ class Group(models.Model):
 
 
 class AssignmentOne(models.Model):
-    group_id = models.CharField(max_length=100, unique=False)
-    assignment_id = models.CharField(max_length=100, unique=False)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+
     key_2 = models.CharField(max_length=100, primary_key=True)
 
 class Assignment(models.Model):
     assignment_id = models.CharField(max_length=100, primary_key=100)
-    assignment_name = models.CharField(max_length=100, null=False)
+
+    assignment_name = models.CharField(max_length=100)
     creation_date = models.DateField(auto_now=True)
     due_date = models.DateField(auto_now=False)
     completion = models.BooleanField()
     qset_id = models.CharField(max_length=100, null=False)
-    class_id = models.CharField(max_length=100, null=False)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
 
 class QuestionOne(models.Model):
-    question_id = models.CharField(max_length=100, null=False)
-    qset_id = models.CharField(max_length=100, null=False)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    qset_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+
     key_3 = models.CharField(max_length=100, primary_key=True)
     
 class Question(models.Model):
@@ -84,12 +85,13 @@ class Question(models.Model):
 
 class Class(models.Model):
     class_id = models.CharField(max_length=100, primary_key=True)
-    teacher_id = models.CharField(max_length=100, null = False)
+    teacher_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     professor_name = models.CharField(max_length=100, null = False)
     class_name = models.CharField(max_length=100, null = False)
 
 class Eval_set(models.Model):
-    eval_set_id = models.CharField(max_length=100, primary_key=True)
+    eval_set_id = models.OneToOneField(Grader)
     student_name = models.CharField(max_length=100, null = False)
 
 class Grader(models.Model):
@@ -99,8 +101,8 @@ class Grader(models.Model):
     key_5 = models.CharField(max_length=100, primary_key=True)
 
 class GraderOne(models.Model):
-    grader_id = models.CharField(max_length=100, null = False)
-    assignment_id = models.CharField(max_length=100, null = False)
+    grader_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    assignment_id = models.ForeignKey(Grader, on_delete=models.CASCADE)
     key_4 = models.CharField(max_length=100, primary_key=True)
 
 
