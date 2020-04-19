4a54db62dbf0956ed89083ece11e6c3391be1a0d
David Kim <kimbyi@bc.edu>
2020-04-18 21:30:38 -0400

    update to database: evalset gone. modified question

--- DLL_Delivery4/peer_assessment/admin.py
+++ DLL_Delivery4/peer_assessment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, GroupOne, Group, AssignmentOne, Assignment, Question, QuestionOne, Class, Eval_set, GraderOne, Grader, Instructors, InstructorSet
+from .models import User, GroupOne, Group, AssignmentOne, Assignment, Question, QuestionOne, Class, GraderOne, Grader, Instructors, InstructorSet
 # Register your models here.
 admin.site.register(User)
 admin.site.register(GroupOne)
@@ -9,7 +9,7 @@ admin.site.register(Assignment)
 admin.site.register(Question)
 admin.site.register(QuestionOne)
 admin.site.register(Class)
-admin.site.register(Eval_set)
+#admin.site.register(Eval_set)
 admin.site.register(GraderOne)
 admin.site.register(Grader)
 admin.site.register(InstructorSet)
