d4a31b4909dbea1a49ee45939fe0ceba325b6188
David Kim <kimbyi@bc.edu>
2020-04-25 21:24:52 -0400

    released peer assessments

--- DLL_Delivery4/frontend/src/containers/Completed.js
+++ DLL_Delivery4/frontend/src/containers/Completed.js
@@ -1,131 +1,127 @@
-import React, {Component} from 'react'
-import Moment from 'moment'
+import React, { Component } from "react";
+import Moment from "moment";
 
 // HomePage
-import Assesments from '../components/DashboardStudent/Completed'
-import axios from 'axios'
-
-
+import Assesments from "../components/DashboardStudent/Completed";
+import axios from "axios";
 
 // Nav
-import Nav from '../components/NavBar'
+import Nav from "../components/NavBar";
 
-import {Redirect} from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 
+const assesmentsCompleted = [
+  {
+    name: "Delivery 1 Assesments",
+    dueDate: Moment(new Date()).subtract(30, "days").calendar(),
+    overAll: 3,
+    teachersComment: "Do you even code brah?",
+  },
+  {
+    name: "Delivery 2 Assesments",
+    dueDate: Moment(new Date()).subtract(20, "days").calendar(),
+    overAll: 3,
+    teachersComment: "When are you dropping?",
+  },
+  {
+    name: "Delivery 3 Assesments",
+    dueDate: Moment(new Date()).subtract(15, "days").calendar(),
+    overAll: 3,
+    teachersComment: "Thoughts on millitary school?",
+  },
+  {
+    name: "Delivery 4 Assesments",
+    dueDate: Moment(new Date()).subtract(5, "days").calendar(),
+    overAll: 2,
+    teachersComment: "You gucci?",
+  },
+];
 
+class StudentHome extends Component {
+  state = {
+    logout: false,
+    changePassword: false,
+  };
+  changePassword = () => {
+    console.log("changepwd");
+    this.setState({
+      changePassword: true,
+    });
+  };
 
-const assesmentsCompleted=[
-    {
-        name:'Delivery 1 Assesments',
-        dueDate:  Moment(new Date()).subtract(30, 'days').calendar(),
-        overAll: 3,
-        teachersComment:'Do you even code brah?'
-    },
-    {
-        name:'Delivery 2 Assesments',
-        dueDate:  Moment(new Date()).subtract(20, 'days').calendar(),
-        overAll: 3,
-        teachersComment:'When are you dropping?'
-    },
-    {
-        name:'Delivery 3 Assesments',
-        dueDate:  Moment(new Date()).subtract(15, 'days').calendar(),
-        overAll: 3,
-        teachersComment:'Thoughts on millitary school?'
-    },
-    {
-        name:'Delivery 4 Assesments',
-        dueDate:  Moment(new Date()).subtract(5, 'days').calendar(),
-        overAll: 2,
-        teachersComment:'You gucci?'
-    }
-]
+  onLogout = () => {
+    console.log("here");
+    this.setState({
+      logout: true,
+    });
+  };
+  render() {
+    //write a get request to get all assessments!!!!!
+    //Http Request
+    console.log("Load Student Completed Assessments");
 
-
-
-
-
-class StudentHome extends Component{
-
-    state={
-        logout:false,
-        changePassword:false
-    }
-    changePassword=()=>{
-        console.log('changepwd')
-        this.setState({
-            changePassword:true
-        })
-    };
-
-    onLogout=()=>{
-        console.log('here')
-        this.setState({
-            logout:true
-        })
-    }
-    render(){
-      //write a get request to get all assessments!!!!!
-        //Http Request
-        console.log("Load Student Completed Assessments")
-
-        //--------------------------------------------------------------------
-        //function to get the cookie from req in order to handle the csrf token
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie !== '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = cookies[i].trim();
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-                }
-            }
-            return cookieValue;
+    //--------------------------------------------------------------------
+    //function to get the cookie from req in order to handle the csrf token
+    function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie !== "") {
+        var cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === name + "=") {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
         }
-        //get csrf token in order to not have request blocked
-        var csrftoken = getCookie('csrftoken');
-        //--------------------------------------------------------------------
+      }
+      return cookieValue;
+    }
+    //get csrf token in order to not have request blocked
+    var csrftoken = getCookie("csrftoken");
+    //--------------------------------------------------------------------
 
-        console.log("View Student Completed Assessments")
-        //Using axios to write post request to Django server that is handled in requestHandler.py to validate
-        axios.get('/studentcompletedassessments/',{
-            email: localStorage.getItem('userEmail'),
-            type: localStorage.getItem('userType')
+    console.log("View Student Completed Assessments");
+    //Using axios to write post request to Django server that is handled in requestHandler.py to validate
+    axios
+      .post(
+        "/studentcompletedassessments/",
+        {
+          email: localStorage.getItem("userEmail"),
+          type: localStorage.getItem("userType"),
         },
         {
-            headers: {
-                'X-CSRFToken': csrftoken
-            }
-        }).then((response) => {
-              var data = response.data
-              console.log("responded to get request");
-              console.log(response.data);
-        }, (error) => {
+          headers: {
+            "X-CSRFToken": csrftoken,
+          },
+        }
+      )
+      .then(
+        (response) => {
+          var data = response.data;
+          console.log("responded to get request");
+          console.log(response.data);
+        },
+        (error) => {
           console.log(error);
-        });
-        //--------------------------------------------------------------------
+        }
+      );
+    //--------------------------------------------------------------------
 
-      
-        return(
-            <Nav
-                user="Student"
-                onLogout={this.onLogout}
-                changePassword={this.changePassword}
-            >
-                <Assesments
-                completedArr={assesmentsCompleted}
-                />
-            {this.state.changePassword===true?<Redirect to='/changepassword' />:null}
-            {this.state.logout===true?<Redirect to='/login' />:null}
-            
-
-            </Nav>
-        )
-    }
+    return (
+      <Nav
+        user="Student"
+        onLogout={this.onLogout}
+        changePassword={this.changePassword}
+      >
+        <Assesments completedArr={assesmentsCompleted} />
+        {this.state.changePassword === true ? (
+          <Redirect to="/changepassword" />
+        ) : null}
+        {this.state.logout === true ? <Redirect to="/login" /> : null}
+      </Nav>
+    );
+  }
 }
 
-export default StudentHome
+export default StudentHome;
--- DLL_Delivery4/peer_assessment/studentpeerassessments.py
+++ DLL_Delivery4/peer_assessment/studentpeerassessments.py
@@ -8,8 +8,8 @@ from django.core import serializers
 # from snippets.models import Snippet
 # from snippets.serializers import SnippetSerializer
 
-#handles displaying students peer assessments
-#Also handles csrf token in order to allow the request to go through
+# handles displaying students peer assessments
+# Also handles csrf token in order to allow the request to go through
 @requires_csrf_token
 @api_view(['POST'])
 def view_assessments(request):
@@ -23,76 +23,87 @@ def view_assessments(request):
     # selectedClass = data.get("selectedClass")
     selectedClass = 1
 
-
-
-    
-    student = User.objects.get(email=email)
-    grader = Grade.objects.filter(grader=student)
-    ids = list()
-    for i in grader:
-        ids.append(i.assessment_question.id)
-    assess_q = Assessment_Question.objects.filter(pk__in=ids)
-    
-    ids2 = list()
-    for i in assess_q:
-        ids2.append(i.assessment_id.id)
-    a = Assessment.objects.filter(pk__in=ids2)
-    assessments = serializers.serialize('json', a)
-    
-    print(a)
-
-    print("DDD")
-
-    c = Class.objects.get(pk=selectedClass)
-
-    assessment_for_specific_class = a.filter(class_id=c)
-    print(assessment_for_specific_class)
-
-    
-
-
-
-    b="F"
-    #Try to write to database to add assessment to list, dummy code in place
+    b = "F"
+    # Try to write to database to add assessment to list, dummy code in place
     try:
-        # user = User.objects.get(email=email, password=currentPassword)
-        b="t"
+        student = User.objects.get(email=email)
+        grader = Grade.objects.filter(grader=student)
+        ids = list()
+        for i in grader:
+            ids.append(i.assessment_question.id)
+        assess_q = Assessment_Question.objects.filter(pk__in=ids)
+
+        ids2 = list()
+        for i in assess_q:
+            ids2.append(i.assessment_id.id)
+        a = Assessment.objects.filter(pk__in=ids2)
+        assessments = serializers.serialize('json', a)
+
+        c = Class.objects.get(pk=selectedClass)
+
+        assess_for_class = a.filter(class_id=c)
+        assess_for_class_json = serializers.serialize('json', assess_for_class)
+        print(assess_for_class_json)
+        b = "t"
     except:
         b = "F"
     print(b)
     return Response(b, status=status.HTTP_200_OK)
 
-#handles displaying students completed assessments
-#Also handles csrf token in order to allow the request to go through
+# handles displaying students completed assessments
+# Also handles csrf token in order to allow the request to go through
 @requires_csrf_token
-@api_view(['GET'])
+@api_view(['POST'])
 def view_completed_assessments(request):
     """
     List all code snippets, or create a new snippet.
     """
+    print("in view_completed_Assess")
+
     data = request.data
     email = data.get("email")
     t = data.get("type")
     print(data)
+    selectedClass = 1
 
-    b="F"
-    #Try to write to database to add assessment to list, dummy code in place
+    b = "F"
+    # Try to write to database to add assessment to list, dummy code in place
     try:
-        # user = User.objects.get(email=email, password=currentPassword)
-        b="t"
+        student = User.objects.get(email=email)
+        grader = Grade.objects.filter(grader=student)
+        ids = list()
+        for i in grader:
+            ids.append(i.assessment_question.id)
+        assess_q = Assessment_Question.objects.filter(pk__in=ids)
+
+        ids2 = list()
+        for i in assess_q:
+            ids2.append(i.assessment_id.id)
+        a = Assessment.objects.filter(pk__in=ids2)
+        assessments = serializers.serialize('json', a)
+
+        c = Class.objects.get(pk=selectedClass)
+
+        assessment_for_specific_class = a.filter(class_id=c)
+        completed = assessment_for_specific_class.filter(released=True)
+        print(completed)
+        completed_assessments = serializers.serialize('json', completed)
+        print(completed_assessments)
+        b = "t"
     except:
         b = "F"
     print(b)
     return Response(b, status=status.HTTP_200_OK)
 
-#Code ran when the student does assessment
-#Also handles csrf token in order to allow the request to go through
+# Code ran when the student does assessment
+# Also handles csrf token in order to allow the request to go through
 @requires_csrf_token
 @api_view(['POST'])
 def student_grade(request):
     """
     List all code snippets, or create a new snippet.
     """
+    print("in student_grade")
     data = request.data
     toDoIndex = data.get("toDoIndex")
     todoSelected = data.get("todoSelected")
@@ -103,11 +114,11 @@ def student_grade(request):
     print(toDoIndex)
     print(todoSelected)
 
-    b="F"
-    #Try to write to database to add assessment to list, dummy code in place
+    b = "F"
+    # Try to write to database to add assessment to list, dummy code in place
     try:
         # user = User.objects.get(email=email, password=currentPassword)
-        b="t"
+        b = "t"
     except:
         b = "F"
     print(b)
