f3faf1ae97e7bf5fa5af057299df32f80fcb565b
David Kim <kimbyi@bc.edu>
2020-04-29 17:02:12 -0400

    question  servlets

--- DLL_Delivery4/peer_assessment/allassessmentsprof.py
+++ DLL_Delivery4/peer_assessment/allassessmentsprof.py
@@ -10,10 +10,10 @@ from django.core import serializers
 import datetime
 import json
 
-#Gets the right assessments to be displayed on Professor assessment page
-#Also handles csrf token in order to allow the request to go through
+# Gets the right assessments to be displayed on Professor assessment page
+# Also handles csrf token in order to allow the request to go through
 
-#Complete (?)
+# Complete (?)
 @requires_csrf_token
 @api_view(['POST'])
 def view_assessments(request):
@@ -21,12 +21,15 @@ def view_assessments(request):
     List all code snippets, or create a new snippet.
     """
     print("CALLING VIEW ASSESSMENTS")
+
     data = request.data
+    print(data)
     email = data.get("email")
     t = data.get("type")
     selectedClass = data.get("selectedClass")
+
     c = Class.objects.get(pk=selectedClass)
-    a = Assessment.objects.filter(class_id = c,)
+    a = Assessment.objects.filter(class_id=c,)
     closed = list()
     open = list()
     for i in a:
@@ -36,23 +39,23 @@ def view_assessments(request):
             closed.append(i)
     openassessments = serializers.serialize('json', open)
     closedassessments = serializers.serialize('json', closed)
-    print(openassessments)
-    print(closedassessments)
+    # print(openassessments)
+    # print(closedassessments)
     # print(assessments)
 
-    b="F"
-    #Try to write to database to add assessment to list, dummy code in place
+    b = "F"
+    # Try to write to database to add assessment to list, dummy code in place
     try:
         # user = User.objects.get(email=email, password=currentPassword)
-        b="t"
+        b = "t"
     except:
         b = "F"
     print(b)
-    return Response([openassessments,closedassessments], status=status.HTTP_200_OK)
+    return Response([openassessments, closedassessments], status=status.HTTP_200_OK)
 
 
-#Code ran when the professor does add assessment
-#Also handles csrf token in order to allow the request to go through
+# Code ran when the professor does add assessment
+# Also handles csrf token in order to allow the request to go through
 @requires_csrf_token
 @api_view(['POST'])
 def add_assessment(request):
@@ -71,7 +74,7 @@ def add_assessment(request):
     print(data)
     print(selectedClass)
     # -------------------------------------------
-    b="F"
+    b = "F"
     c = Class.objects.get(pk=selectedClass)
     class_groups = Group.objects.filter(class_id=c)
 
@@ -81,37 +84,40 @@ def add_assessment(request):
     qs = Question.objects.all()
     assess_qs = list()
     for q in qs:
-        assess_q = Assessment_Question.objects.create(assessment_id=assessment, question_id=q)
+        assess_q = Assessment_Question.objects.create(
+            assessment_id=assessment, question_id=q)
         assess_q.save()
         assess_qs.append(assess_q)
 
     for group in class_groups:
-        g_a= Group_Assessment.objects.create(group_id=group, assessment_id=assessment)
+        g_a = Group_Assessment.objects.create(
+            group_id=group, assessment_id=assessment)
         g_a.save()
-        group_students = Group_Student.objects.filter(group_id=group).values_list('student_id',flat=True)
+        group_students = Group_Student.objects.filter(
+            group_id=group).values_list('student_id', flat=True)
         students = User.objects.filter(pk__in=group_students)
         print(students)
         for grader in students:
             for gradee in students:
                 for aq in assess_qs:
-                    grade = Grade.objects.create(grader=grader, gradee=gradee, assessment_question=aq)
+                    grade = Grade.objects.create(
+                        grader=grader, gradee=gradee, assessment_question=aq)
                     grade.save()
     print(assessment)
-    assess = Assessment.objects.get(pk = assessment.id)
+    assess = Assessment.objects.get(pk=assessment.id)
     a = serializers.serialize('json', [assess, ])
-    #Try to write to database to add assessment to list, dummy code in place
+    # Try to write to database to add assessment to list, dummy code in place
     try:
 
-
-        b="t"
+        b = "t"
     except:
         b = "F"
     print(b)
     print(data)
     return Response(a, status=status.HTTP_200_OK)
 
-#Code ran when the professor does grades assessment
-#Also handles csrf token in order to allow the request to go through
+# Code ran when the professor does grades assessment
+# Also handles csrf token in order to allow the request to go through
 @requires_csrf_token
 @api_view(['POST'])
 def deadline_update(request):
@@ -130,15 +136,52 @@ def deadline_update(request):
     assessment = Assessment.objects.get(pk=int(pk))
     assessment.due_date = dueDate
     assessment.save()
-    b="F"
+    b = "F"
     assess = Assessment.objects.get(pk=assessment.id)
     a = serializers.serialize('json', [assess, ])
-    #Try to write to database to add assessment to list, dummy code in place
+    # Try to write to database to add assessment to list, dummy code in place
     try:
         # user = User.objects.get(email=email, password=currentPassword)
-        b="t"
+        b = "t"
     except:
         b = "F"
     print(b)
     print(data)
     return Response(a, status=status.HTTP_200_OK)
+
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+@requires_csrf_token
+@api_view(['POST'])
+def add_question(request):
+    data = request.data
+    q_type = data.get("type")
+    q = data.get("question")
+    question = Question.objects.create(question=q, type=q_type)
+    question.save()
+
+    
+
+    try:
+        b = "t"
+    except:
+        b = "F"
+
+    print(b)
+    print(data)
+    return Response(a, status=status.HTTP_200_OK)
+
+
+@requires_csrf_token
+@api_view(['POST'])
+def view_all_questions(request):
+    print("VIEW QUESTIONS")
+    try:
+        q = Question.objects.all()
+        questions = serializers.serialize('json', q)
+        b = "t"
+    except:
+        b = "F"
+    print(b)
+    return Response(questions, status=status.HTTP_200_OK)
